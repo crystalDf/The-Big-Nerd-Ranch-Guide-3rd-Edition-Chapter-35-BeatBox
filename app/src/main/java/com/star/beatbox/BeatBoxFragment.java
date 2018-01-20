91dc735b3b65c8d06dad1c56ca8ac2f1eb073f7d
crystalDf <chendong333@gmail.com>
2018-01-20 22:26:06 +0800

    Chapter 35 BeatBox

--- app/src/main/java/com/star/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/star/beatbox/BeatBoxFragment.java
@@ -1,7 +1,11 @@
 package com.star.beatbox;
 
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.databinding.DataBindingUtil;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatSeekBar;
@@ -9,7 +13,9 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.SeekBar;
 
 import com.star.beatbox.databinding.FragmentBeatBoxBinding;
@@ -22,6 +28,7 @@ public class BeatBoxFragment extends Fragment {
     private static final int SEEK_BAR_MAX = 200;
 
     private BeatBox mBeatBox;
+    private View mRedFill;
 
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
@@ -86,22 +93,44 @@ public class BeatBoxFragment extends Fragment {
             }
         });
 
+        mRedFill = fragmentBeatBoxBinding.fragmentBeatBoxRedFill;
+        mRedFill.setVisibility(View.INVISIBLE);
+
         return fragmentBeatBoxBinding.getRoot();
     }
 
     private class SoundHolder extends RecyclerView.ViewHolder {
         private ListItemSoundBinding mListItemSoundBinding;
+        private SoundViewModel mSoundViewModel;
+        private Button mButton;
 
         public SoundHolder(ListItemSoundBinding listItemSoundBinding) {
             super(listItemSoundBinding.getRoot());
 
+            mSoundViewModel = new SoundViewModel(mBeatBox);
+
             mListItemSoundBinding = listItemSoundBinding;
-            mListItemSoundBinding.setSoundViewModel(new SoundViewModel(mBeatBox));
+            mListItemSoundBinding.setSoundViewModel(mSoundViewModel);
+
+            mButton = mListItemSoundBinding.listItemSoundButton;
         }
 
         public void bindSound(Sound sound) {
             mListItemSoundBinding.getSoundViewModel().setSound(sound);
             mListItemSoundBinding.executePendingBindings();
+
+            mButton.setOnClickListener(v -> {
+                int[] clickCoords = new int[2];
+
+                v.getLocationOnScreen(clickCoords);
+
+                clickCoords[0] += v.getWidth() / 2;
+                clickCoords[1] += v.getHeight() / 2;
+
+                performRevealAnimation(mRedFill, clickCoords[0], clickCoords[1]);
+
+                mSoundViewModel.onButtonClicked();
+            });
         }
     }
 
@@ -139,5 +168,45 @@ public class BeatBoxFragment extends Fragment {
         super.onDestroy();
 
         mBeatBox.release();
+    }
+
+    private void performRevealAnimation(final View view, int screenCenterX, int screenCenterY) {
+        int[] animatingViewCoords = new int[2];
+
+        view.getLocationOnScreen(animatingViewCoords);
+
+        int centerX = screenCenterX - animatingViewCoords[0];
+        int centerY = screenCenterY - animatingViewCoords[1];
+
+        Point size = new Point();
+
+        if (getActivity() == null) {
+            return;
+        }
+
+        getActivity().getWindowManager().getDefaultDisplay().getSize(size);
+
+        int maxRadius = size.y;
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+
+        } else {
+            view.setVisibility(View.VISIBLE);
+
+            Animator animator = ViewAnimationUtils.createCircularReveal(
+                    view, centerX, centerY, 0, maxRadius
+            );
+
+            animator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+
+                    view.setVisibility(View.INVISIBLE);
+                }
+            });
+
+            animator.start();
+        }
     }
 }
